7771b9021cf5289129a328f12ff28bbe43eddebd
Max Fierro <elmasse@gmail.com>
2018-01-24 16:51:54 -0300

    first commit

---
+++ taskfile.js
@@ -0,0 +1,21 @@
+
+export async function compile(task, opts) {
+  await task.source(opts.src || 'src/**/*.js').babel().target('dist/')
+}
+
+export async function build(task) {
+  await task.serial(['lint',  'compile'])
+}
+
+export async function lint(task) {
+  await task.source('src/**/*.js').standard();
+}
+
+export default async function (task) {
+  await task.start('build')
+  await task.watch('src/**/*.js', ['lint','compile'])
+}
+
+export async function release (task) {
+  await task.clear('dist').start('build')
+}
