ea4f9acf0f90788397d9846d9d56d6e25aaede9f
Max Fierro <elmasse@gmail.com>
2018-01-26 12:10:46 -0300

    improved bundle generation. Added back es module

--- taskfile.js
+++ taskfile.js
@@ -1,42 +1,51 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
+
+const baseRollupPlugins = [
+  babel({
+    exclude: 'node_modules/**',
+    runtimeHelpers: true 
+  }),
+  commonjs({
+    include: 'node_modules/**',
+    namedExports: {
+      'react': ['Children', 'Component', 'PropTypes', 'createElement'],
+      'react-dom': ['render', 'findDOMNode']
+    }      
+  }),
+  resolve({
+    jsnext: true,
+    main: true
+  })
+]
+
+const externals = [
+  'react',
+  'react-dom',
+  'emotion',
+  'frontmatter',
+  'hastscript',
+  'highlight.js',
+  'react-emotion',
+  'rehype-raw',
+  'rehype-react',
+  'remark-emoji',
+  'remark-parse',
+  'remark-rehype',
+  'unified',
+  'unist-builder'
+]
 
 export async function cjs(task, opts) {
   await task
     .source('src/index.js')
     .rollup({
-      plugins: [
-        babel({
-          exclude: 'node_modules/**',
-          runtimeHelpers: true 
-        }),
-        commonjs({
-          include: 'node_modules/**',
-          namedExports: {
-            'react': ['Children', 'Component', 'PropTypes', 'createElement'],
-            'react-dom': ['render', 'findDOMNode']
-          }      
-        }),
-        resolve({
-          jsnext: true,
-          main: true
-        }),
-        json({
-          include: 'node_modules/**'      
-        })            
-      ],
-      external: [
-        'react',
-        'react-dom',
-        'prop-types'
-      ],  
+      plugins: baseRollupPlugins,
+      external: externals,  
       output: {
         file: 'postcast.js',
-        globals: { react: 'React', 'prop-types': 'PropTypes', preact: 'preact' },      
         format: 'cjs'
       }
     })
@@ -47,33 +56,10 @@ export async function es(task, opts) {
   await task
     .source('src/index.js')
     .rollup({
-      plugins: [
-        babel({
-          exclude: 'node_modules/**'
-        }),
-        commonjs({
-          include: 'node_modules/**',
-          namedExports: {
-            'react': ['Children', 'Component', 'PropTypes', 'createElement'],
-            'react-dom': ['render', 'findDOMNode']
-          }      
-        }),
-        resolve({
-          jsnext: true,
-          main: true
-        }),
-        json({
-          include: 'node_modules/**'      
-        })            
-      ],
-      external: [
-        'react',
-        'react-dom',
-        'prop-types'
-      ],  
+      plugins: baseRollupPlugins,
+      external: externals,  
       output: {
         file: 'es/postcast.js',
-        globals: { react: 'React', 'prop-types': 'PropTypes', preact: 'preact' },      
         format: 'es'
       }
     })
@@ -81,7 +67,7 @@ export async function es(task, opts) {
 }
 
 export async function compile(task) {
-  await task.parallel(['cjs'])
+  await task.parallel(['cjs', 'es'])
 }
 
 export async function build(task) {
