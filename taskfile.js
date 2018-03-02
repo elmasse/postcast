0929fa97307081c0337cf0a434881171f08073e6
Max Fierro <elmasse@gmail.com>
2018-03-02 19:36:14 -0300

    added new deps to externals

--- taskfile.js
+++ taskfile.js
@@ -12,7 +12,7 @@ const baseRollupPlugins = [
     namedExports: {
       'react': ['Children', 'Component', 'PropTypes', 'createElement'],
       'react-dom': ['render', 'findDOMNode']
-    }      
+    }
   }),
   resolve({
     jsnext: true,
@@ -33,8 +33,10 @@ const externals = [
   'remark-emoji',
   'remark-parse',
   'remark-rehype',
+  'sbd',
   'unified',
-  'unist-builder'
+  'unist-builder',
+  'unist-visit',
 ]
 
 export async function cjs(task, opts) {
