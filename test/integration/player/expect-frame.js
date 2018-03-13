9457a49fa778cabb4422db5fa3781744182cf186
Max Fierro <elmasse@gmail.com>
2018-03-13 20:17:51 -0300

    added more test for processor and new matcher

--- test/integration/player/expect-frame.js
+++ test/integration/player/expect-frame.js
@@ -1,7 +1,13 @@
 /* global expect */
 
+expect.extend({
+  toHaveCaptionWithText,
+  toHaveCaptionWith,
+  toHaveContentWithText
+})
 
-const toHaveContentWithText = (actual, contentText) => {
+
+function toHaveContentWithText (actual, contentText) {
   const content = actual.props.children[0]
   const element = content.props.children[0]
   const value = element.props.children[0]
@@ -14,20 +20,40 @@ const toHaveContentWithText = (actual, contentText) => {
   
 }
 
-const toHaveCaptionWithText = (actual, captionText) => {
+function toHaveCaptionWithText (actual, captionText) {
   const caption = actual.props.children[1]
   const p = caption.props.children[0]
   const value = p.props.children[0]
   const pass = actual && caption && p && value === captionText
 
   return ({
-    message: () => `expected "${value}" in frame ${index} caption to${pass ? ' not' : '' } be "${captionText}"`,
+    message: () => `expected "${value}" in frame caption to${pass ? ' not' : '' } be "${captionText}"`,
     pass
   })
 }
 
+function toHaveCaptionWith (actual, expectedArray) {
+  const caption = actual.props.children[1]
+  const p = caption.props.children[0]
+  const descendants = p.props.children
+  let pass = true
+  let msg;
+  expectedArray.forEach((expected, index) => {
+    if (typeof expected === 'string' && descendants[index] !== expected) {
+      pass = false
+    }
+    if (typeof expected === 'object') {
+      for (let [k,v] in Object.entries(expected)) {
+        if (descendants[index][k] !== v) {
+          pass = false
+        }
+      }
+    }
+  });
+  return ({
+    message: () => `caption is wrong:`,
+    pass
+  })
 
-expect.extend({
-  toHaveCaptionWithText,
-  toHaveContentWithText
-})
+
+}
--- test/integration/player/processor.test.js
+++ test/integration/player/processor.test.js
@@ -38,7 +38,53 @@ describe('player/processor content', () => {
     expect(frames[1]).toHaveCaptionWithText(title)
     expect(frames[2]).toHaveCaptionWithText(sentences[0] )
     expect(frames[3]).toHaveCaptionWithText(sentences[1] )
-  })  
+  })
+  test('process content with title and paragraph with 2 sentences with line break <br/>', () => {
+    const sentences = ['Some text.', 'Another sentence.']
+    const paragraph = sentences.join('<br/>')
+    const title = 'title'
+    const markdown = create({ content: `# ${title}\n${paragraph}` })    
+
+    //when
+    const { content: frames } = processor(markdown)
+
+    //expect
+    expect(frames.length).toBe(4)
+    expect(frames[0]).toHaveContentWithText(title)
+    expect(frames[1]).toHaveCaptionWithText(title)
+    expect(frames[2]).toHaveCaptionWithText(sentences[0] )
+    expect(frames[3]).toHaveCaptionWithText(sentences[1] )
+  })
+  test('process content with title and paragraph with 2 sentences with line break: 2 spaces and return', () => {
+    const sentences = ['Some text.', 'Another sentence.']
+    const paragraph = sentences.join('  \n')
+    const title = 'title'
+    const markdown = create({ content: `# ${title}\n${paragraph}` })    
+
+    //when
+    const { content: frames } = processor(markdown)
+
+    //expect
+    expect(frames.length).toBe(4)
+    expect(frames[0]).toHaveContentWithText(title)
+    expect(frames[1]).toHaveCaptionWithText(title)
+    expect(frames[2]).toHaveCaptionWithText(sentences[0] )
+    expect(frames[3]).toHaveCaptionWithText(sentences[1] )
+  })
+  test('process content with title and paragraph with a link', () => {
+    const paragraph = `Some text with [a link](link).`
+    const title = 'title'
+    const markdown = create({ content: `# ${title}\n${paragraph}` })    
+
+    //when
+    const { content: frames } = processor(markdown)
+
+    //expect
+    expect(frames.length).toBe(3)
+    expect(frames[0]).toHaveContentWithText(title)
+    expect(frames[1]).toHaveCaptionWithText(title)
+    expect(frames[2]).toHaveCaptionWith(['Some text with ', { type: 'a' }, '.'])
+  })
 });
 
 describe('player/processor with YAML frontmatter', () => {
